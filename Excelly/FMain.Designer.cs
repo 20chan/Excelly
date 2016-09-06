3f1f90dad0b54b2af431ab2d7ac75d567fb1c108
philly <sonagi.philly@gmail.com>
2016-09-06 14:54:41 +0900

    초기 커밋.

---
+++ Excelly/FMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Excelly
+{
+    partial class FMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNew = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cellTextbox = new System.Windows.Forms.ToolStripTextBox();
+            this.변수관리ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.ColumnNew});
+            this.dgvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvMain.Location = new System.Drawing.Point(0, 27);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowTemplate.Height = 23;
+            this.dgvMain.Size = new System.Drawing.Size(777, 519);
+            this.dgvMain.TabIndex = 0;
+            this.dgvMain.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dgvMain_CellBeginEdit);
+            this.dgvMain.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMain_CellEndEdit);
+            this.dgvMain.SelectionChanged += new System.EventHandler(this.dgvMain_SelectionChanged);
+            this.dgvMain.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgvMain_KeyDown);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "A";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "B";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "C";
+            this.Column3.Name = "Column3";
+            // 
+            // ColumnNew
+            // 
+            this.ColumnNew.HeaderText = "+";
+            this.ColumnNew.Name = "ColumnNew";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cellTextbox,
+            this.변수관리ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(777, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cellTextbox
+            // 
+            this.cellTextbox.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.cellTextbox.AutoSize = false;
+            this.cellTextbox.Name = "cellTextbox";
+            this.cellTextbox.Size = new System.Drawing.Size(600, 23);
+            this.cellTextbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cellTextbox_KeyDown);
+            // 
+            // 변수관리ToolStripMenuItem
+            // 
+            this.변수관리ToolStripMenuItem.Name = "변수관리ToolStripMenuItem";
+            this.변수관리ToolStripMenuItem.Size = new System.Drawing.Size(71, 23);
+            this.변수관리ToolStripMenuItem.Text = "변수 관리";
+            this.변수관리ToolStripMenuItem.Click += new System.EventHandler(this.변수관리ToolStripMenuItem_Click);
+            // 
+            // FMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 546);
+            this.Controls.Add(this.dgvMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripTextBox cellTextbox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNew;
+        private System.Windows.Forms.ToolStripMenuItem 변수관리ToolStripMenuItem;
+        public System.Windows.Forms.DataGridView dgvMain;
+    }
+}
+
---
+++ Excelly/FMain.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Excelly
+{
+    public partial class FMain : Form
+    {
+        private Dictionary<Point, string> funcCells = new Dictionary<Point, string>();
+        private readonly string[] funcNames = new string[]
+        {
+            "AVERAGE", "SUM", "RAND", "RANDDOUBLE", "VAR", "DEV"
+        };
+        public FVars varsManager;
+        
+        public FMain()
+        {
+            InitializeComponent();
+            SetRowsAndCells();
+            varsManager = new FVars(this);
+        }
+
+        private void SetRowsAndCells()
+        {
+            this.dgvMain.Rows.Add(3);
+        }
+
+        public static string[] GetArgValue(string args, FMain form)
+        {
+            List<string> result = new List<string>();
+            foreach (string arg in args.Split(null))
+            {
+                if (arg == "")
+                    continue;
+                else if (arg.StartsWith("{") && arg.EndsWith("}"))
+                {
+                    result.AddRange(form.varsManager.GetVars(arg.Substring(1, arg.Length - 2)));
+                }
+                else if (arg.StartsWith("'") && arg.EndsWith("'"))
+                {
+                    result.Add(arg.Substring(1, arg.Length - 2));
+                }
+                else if (arg.Length == 1 && char.IsLetter(arg[0]))
+                {
+                    result.AddRange(form.AllRowCells(arg).Select(a => GetArgValue(a, form)[0]));
+                }
+                else
+                {
+                    int x = Convert.ToChar(arg[0]) - 'A', y = Convert.ToInt32(arg.Substring(1));
+                    result.Add(form.dgvMain.Rows[y].Cells[x].Value.ToString());
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public string[] AllRowCells(string column)
+        {
+            string[] result = new string[dgvMain.Rows.Count-1];
+            for (int i = 0; i < dgvMain.Rows.Count - 1; i++)
+                result[i] = column + i.ToString();
+            return result;
+        }
+
+        private string CalcFunc(string s)
+        {
+            string function = s.Substring(1, s.IndexOf("(") - 1);
+            string _args = s.Substring(s.IndexOf("(") + 1, s.IndexOf(")") - s.IndexOf("(") - 1);
+            string[] args = GetArgValue(_args, this);
+
+            return CalcFunc(function, args);
+        }
+
+        public static string CalcFunc(string function, string[] args)
+        {
+            if (function == "SUM")
+            {
+                return CellFunc.Sum(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
+            }
+            if (function == "AVERAGE")
+            {
+                return CellFunc.Average(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
+            }
+            if (function == "RAND")
+            {
+                if (args.Length == 0)
+                    return CellFunc.RandInt().ToString();
+                else if (args.Length == 1)
+                    return CellFunc.RandInt(Convert.ToInt32(args[0])).ToString();
+                else
+                    return CellFunc.RandInt(Convert.ToInt32(args[0]), Convert.ToInt32(args[1])).ToString();
+            }
+            if (function == "RANDDOUBLE")
+            {
+                if (args.Length == 0)
+                    return CellFunc.RandDouble().ToString();
+                else if (args.Length == 1)
+                    return CellFunc.RandDouble(Convert.ToDouble(args[0])).ToString();
+                else
+                    return CellFunc.RandDouble(Convert.ToDouble(args[0]), Convert.ToDouble(args[1])).ToString();
+            }
+            if (function == "VAR")
+            {
+                return CellFunc.Variance(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
+            }
+            if (function == "DEV")
+            {
+                return CellFunc.Deviation(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
+            }
+            return "ERROR";
+        }
+
+        /// <summary>
+        /// Include Random
+        /// </summary>
+        private void RefreshFuncCells()
+        {
+            foreach(var key in funcCells.Keys)
+            {
+                dgvMain.Rows[key.Y].Cells[key.X].Value = CalcFunc(funcCells[key]);
+            }
+        }
+
+        /// <summary>
+        /// Exclude Random
+        /// </summary>
+        private void ReCalcCells()
+        {
+            foreach (var key in funcCells.Keys)
+            {
+                if(!funcCells[key].StartsWith("=RAND"))
+                    dgvMain.Rows[key.Y].Cells[key.X].Value = CalcFunc(funcCells[key]);
+            }
+        }
+        
+        private void dgvMain_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if(e.ColumnIndex == dgvMain.Columns.Count - 1)
+            {
+                DataGridViewTextBoxColumn c = new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = ((char)('A' + e.ColumnIndex)).ToString()
+                };
+                dgvMain.Columns.Insert(e.ColumnIndex, c);
+                dgvMain.CurrentCell = dgvMain.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                e.Cancel = true;
+            }
+        }
+
+        private void dgvMain_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dgvMain.Columns.Count - 1)
+            {
+                dgvMain.CurrentCell = dgvMain.Rows[e.RowIndex].Cells[e.ColumnIndex-1];
+                dgvMain.BeginEdit(true);
+            }
+            else
+            {
+                try
+                {
+                    if (dgvMain.CurrentCell.Value.ToString().StartsWith("="))
+                    {
+                        string func = dgvMain.CurrentCell.Value.ToString();
+                        string function = func.Substring(1, func.IndexOf("(") - 1);
+                        if (funcNames.Contains(function))
+                        {
+                            Point p = new Point(e.ColumnIndex, e.RowIndex);
+                            if (!funcCells.ContainsKey(p))
+                                funcCells.Add(p, func);
+                            else
+                                funcCells[p] = func;
+
+                            dgvMain.CurrentCell.Value = CalcFunc(func);
+                        }
+                    }
+                    else
+                    {
+                        if (funcCells.ContainsKey(new Point(e.ColumnIndex, e.RowIndex)))
+                            funcCells.Remove(new Point(e.ColumnIndex, e.RowIndex));
+                    }
+                }
+                catch { }
+                finally { try { ReCalcCells(); } catch { } }
+            }
+        }
+
+        private void dgvMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+                RefreshFuncCells();
+        }
+
+        private void dgvMain_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvMain.SelectedCells.Count == 0) return;
+            Point p = new Point(dgvMain.CurrentCell.ColumnIndex, dgvMain.CurrentCell.RowIndex);
+            if (funcCells.ContainsKey(p))
+                cellTextbox.Text = funcCells[p];
+            else if (dgvMain.CurrentCell.Value != null)
+                cellTextbox.Text = dgvMain.CurrentCell.Value.ToString();
+            else
+                cellTextbox.Text = "";
+        }
+
+        private void cellTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                dgvMain.CurrentCell.Value = cellTextbox.Text;
+                dgvMain_CellEndEdit(dgvMain, new DataGridViewCellEventArgs(dgvMain.CurrentCell.ColumnIndex, dgvMain.CurrentCell.RowIndex));
+            }
+        }
+
+        private void 변수관리ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            varsManager.Show();
+        }
+    }
+}
