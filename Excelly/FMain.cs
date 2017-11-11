681cf6bc5b4878540480e76d87762358dcfc3fd6
phillyai <sonagi.philly@gmail.com>
2017-11-12 00:38:59 +0900

    모든 파일 삭제, 셀 컨트롤 관련 함수는 약간 리팩토링.
    기존의 중복되는 함수나 메인 함수의 애매한 설계를 다시 잡고 간략하게 바꿈.

--- Excelly/FMain.cs
+++ Excelly/FMain.cs
@@ -12,138 +12,35 @@ namespace Excelly
     public partial class FMain : Form
     {
         private Dictionary<Point, string> funcCells = new Dictionary<Point, string>();
-        private readonly string[] funcNames = new string[]
-        {
-            "AVERAGE", "SUM", "RAND", "RANDDOUBLE","POT", "VAR", "DEV"
-        };
-        public FVars varsManager;
         
         public FMain()
         {
             InitializeComponent();
             SetRowsAndCells();
-            varsManager = new FVars(this);
         }
 
         private void SetRowsAndCells()
         {
             this.dgvMain.Rows.Add(3);
         }
-
-        public static string[] GetArgValue(string args, FMain form)
-        {
-            List<string> result = new List<string>();
-            foreach (string arg in args.Split(null))
-            {
-                if (arg == "")
-                    continue;
-                else if (arg.StartsWith("{") && arg.EndsWith("}"))
-                {
-                    result.AddRange(form.varsManager.GetVars(arg.Substring(1, arg.Length - 2)));
-                }
-                else if (arg.StartsWith("'") && arg.EndsWith("'"))
-                {
-                    result.Add(arg.Substring(1, arg.Length - 2));
-                }
-                else if (arg.Length == 1 && char.IsLetter(arg[0]))
-                {
-                    result.AddRange(form.AllRowCells(arg).Select(a => GetArgValue(a, form)[0]));
-                }
-                else
-                {
-                    int x = Convert.ToChar(arg[0]) - 'A', y = Convert.ToInt32(arg.Substring(1));
-                    result.Add(form.dgvMain.Rows[y].Cells[x].Value.ToString());
-                }
-            }
-
-            return result.ToArray();
-        }
-
-        public string[] AllRowCells(string column)
-        {
-            string[] result = new string[dgvMain.Rows.Count-1];
-            for (int i = 0; i < dgvMain.Rows.Count - 1; i++)
-                result[i] = column + i.ToString();
-            return result;
-        }
-
-        private string CalcFunc(string s)
-        {
-            string function = s.Substring(1, s.IndexOf("(") - 1);
-            string _args = s.Substring(s.IndexOf("(") + 1, s.IndexOf(")") - s.IndexOf("(") - 1);
-            string[] args = GetArgValue(_args, this);
-
-            return CalcFunc(function, args);
-        }
-
-        public static string CalcFunc(string function, string[] args)
-        {
-            if (function == "SUM")
-            {
-                return CellFunc.Sum(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
-            }
-            if (function == "AVERAGE")
-            {
-                return CellFunc.Average(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
-            }
-            if (function == "RAND")
-            {
-                if (args.Length == 0)
-                    return CellFunc.RandInt().ToString();
-                else if (args.Length == 1)
-                    return CellFunc.RandInt(Convert.ToInt32(args[0])).ToString();
-                else
-                    return CellFunc.RandInt(Convert.ToInt32(args[0]), Convert.ToInt32(args[1])).ToString();
-            }
-            if (function == "RANDDOUBLE")
-            {
-                if (args.Length == 0)
-                    return CellFunc.RandDouble().ToString();
-                else if (args.Length == 1)
-                    return CellFunc.RandDouble(Convert.ToDouble(args[0])).ToString();
-                else
-                    return CellFunc.RandDouble(Convert.ToDouble(args[0]), Convert.ToDouble(args[1])).ToString();
-            }
-            if(function == "POT")
-            {
-                if (args.Length == 1)
-                    return CellFunc.Potential(Convert.ToDouble(args[0])).ToString();
-                
-            }
-            if (function == "VAR")
-            {
-                return CellFunc.Variance(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
-            }
-            if (function == "DEV")
-            {
-                return CellFunc.Deviation(args.Select(a => Convert.ToDouble(a)).ToArray()).ToString();
-            }
-            return "ERROR";
-        }
-
-        /// <summary>
-        /// Include Random
-        /// </summary>
-        private void RefreshFuncCells()
+        
+        private void RefreshFuncCells(bool includeRandom)
         {
             foreach(var key in funcCells.Keys)
             {
-                dgvMain.Rows[key.Y].Cells[key.X].Value = CalcFunc(funcCells[key]);
+                // TODO: includeRandom이 true일때 랜덤함수를 걸러내야 함
+                if (includeRandom)
+                    dgvMain.Rows[key.Y].Cells[key.X].Value = Execute(funcCells[key]);
             }
         }
 
-        /// <summary>
-        /// Exclude Random
-        /// </summary>
-        private void ReCalcCells()
+        private string Execute(string expression)
         {
-            foreach (var key in funcCells.Keys)
-            {
-                if(!funcCells[key].StartsWith("=RAND"))
-                    dgvMain.Rows[key.Y].Cells[key.X].Value = CalcFunc(funcCells[key]);
-            }
+            throw new NotImplementedException();
         }
-        
+
+        #region 이벤트
+
         private void dgvMain_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
         {
             if(e.ColumnIndex == dgvMain.Columns.Count - 1)
@@ -171,20 +68,7 @@ namespace Excelly
                 {
                     if (dgvMain.CurrentCell.Value.ToString().StartsWith("="))
                     {
-                        string func = dgvMain.CurrentCell.Value.ToString();
-                        Calculator c = new Calculator();
-                        var res = c.ParseCode(func.Substring(1));
-                        string function = func.Substring(1, func.IndexOf("(") - 1);
-                        if (funcNames.Contains(function))
-                        {
-                            Point p = new Point(e.ColumnIndex, e.RowIndex);
-                            if (!funcCells.ContainsKey(p))
-                                funcCells.Add(p, func);
-                            else
-                                funcCells[p] = func;
-
-                            dgvMain.CurrentCell.Value = CalcFunc(func);
-                        }
+                        throw new NotImplementedException("여기에 함수 계산을 추가");
                     }
                     else
                     {
@@ -193,14 +77,14 @@ namespace Excelly
                     }
                 }
                 catch { }
-                finally { try { ReCalcCells(); } catch { } }
+                finally { try { RefreshFuncCells(false); } catch { } }
             }
         }
 
         private void dgvMain_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.F5)
-                RefreshFuncCells();
+                RefreshFuncCells(true);
         }
 
         private void dgvMain_SelectionChanged(object sender, EventArgs e)
@@ -226,7 +110,9 @@ namespace Excelly
 
         private void 변수관리ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            varsManager.Show();
+            throw new NotImplementedException("변수 관리...차응ㄹ..띄우자..우앵우.ㅇ....");
         }
+
+        #endregion
     }
 }
