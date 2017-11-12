10817fc0591625de84f181f07e9390f5e1e41cd8
phillyai <sonagi.philly@gmail.com>
2017-11-12 22:53:15 +0900

    렉서 추가

---
+++ Excelly/Execution/Lexer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Excelly.Execution
+{
+    public class Lexer
+    {
+        public string Code { get; }
+        private int _index = 0;
+
+        public Lexer(string code)
+        {
+            Code = code;
+        }
+
+        private char Current => Code[_index];
+        private bool IsEof => _index == Code.Length;
+
+        private static bool IsWhiteSpace(char c)
+            => c == ' ' || c == '\n' || c == '\r' || c == '\t';
+
+        private static bool IsOperator(char c)
+            => c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+
+        private static bool IsParen(char c)
+            => c == '(' || c == ')';
+
+        public IEnumerable<Token> Parse()
+        {
+            while (!IsEof)
+                yield return GetNextToken();
+        }
+
+        internal Token GetNextToken()
+        {
+            while (IsWhiteSpace(Current)) _index++;
+            if (IsOperator(Current)) return GetOperatorToken();
+            if (char.IsDigit(Current)) return GetNumberToken();
+            if (IsParen(Current)) return GetParenToken();
+
+            throw new Exception("Must not happened");
+        }
+
+        private Token GetOperatorToken()
+        {
+            return new Token(Code[_index++].ToString(), TokenType.Operator);
+        }
+
+        private Token GetNumberToken()
+        {
+            var startIndex = _index;
+            var dotIncluded = false;
+
+            while (!IsEof)
+            {
+                if (Current == '.')
+                {
+                    if (!dotIncluded)
+                        dotIncluded = true;
+                    else
+                        throw new Exception("Number has two dot");
+                }
+                if (!char.IsDigit(Current)) break;
+
+                _index++;
+            }
+
+            return new Token(Code.Substring(startIndex, _index - startIndex), TokenType.Number);
+        }
+
+        private Token GetParenToken()
+        {
+            return new Token(Code[_index++].ToString(), TokenType.Paren);
+        }
+    }
+}
---
+++ Excelly/Execution/Token.cs
@@ -0,0 +1,23 @@
+﻿namespace Excelly.Execution
+{
+    public struct Token
+    {
+        public string Code;
+        public TokenType Type;
+
+        public Token(string code, TokenType type)
+        {
+            Code = code;
+            Type = type;
+        }
+    }
+
+    public enum TokenType
+    {
+        EOF = -1,
+        None = 0,
+        Operator,
+        Number,
+        Paren,
+    }
+}
